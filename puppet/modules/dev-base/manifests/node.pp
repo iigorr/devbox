e826abb96ebbf6fd41ddfc31917b1063f11c1e1d
Igor Lankin <dev@lankin.net>
2014-06-01 01:38:42 +0200

    added grunt-cli (and fixed syntax warning)

--- puppet/modules/dev-base/manifests/node.pp
+++ puppet/modules/dev-base/manifests/node.pp
@@ -3,7 +3,7 @@ class dev-base::node {
   exec { 'node-repo' :
     command => '/usr/bin/add-apt-repository ppa:chris-lea/node.js',
     creates => '/etc/apt/sources.list.d/chris-lea-node_js-precise.list',
-    require => package['python-software-properties']
+    require => Package['python-software-properties']
   }
 
   exec { 'apt-ready' :
@@ -14,5 +14,10 @@ class dev-base::node {
 
   package { [ "nodejs" ] :
     require => Exec["apt-ready"]
+  }
+
+  exec { 'grunt-cli':
+    command => '/usr/bin/npm -g install grunt-cli',
+    creates => '/usr/bin/grunt'
   }
 }
