71369f75d260043aff0ffa6a109278c8f1671746
Igor Lankin <dev@lankin.net>
2014-06-10 18:51:14 +0200

    added .aws directory to user home

--- puppet/modules/dev-base/manifests/aws.pp
+++ puppet/modules/dev-base/manifests/aws.pp
@@ -8,11 +8,15 @@ class dev-base::aws {
     require => [Package["python-pip"]],
   }
 
+  file{"/home/$username/.aws/":
+      ensure => directory,
+  }
   file { "aws-config":
     path    => "/home/$username/.aws/config",
     owner   => "$username",
     group   => "$username",
     mode    => '0600',
     content => template('dev-base/aws/config.erb'),
+    require => File['/home/$username/.aws/']
   }
 }
