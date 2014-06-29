6903d8dfffec19650a1272a744bf695af5db39e9
Igor Lankin <dev@lankin.net>
2014-06-29 16:41:45 +0200

    added setup of neo4j server

--- puppet/modules/dev-base/manifests/init.pp
+++ puppet/modules/dev-base/manifests/init.pp
@@ -1,20 +1,17 @@
 
 class dev-base {
 
-  $basics = [ "screen", "strace", "curl", "wget", "telnet", "python", "python-pip"]
+  $basics = [ 'screen', 'strace', 'curl', 'wget', 'telnet', 'python', 'python-pip', 'git-core', 'neo4j']
   package { $basics:
-    ensure => "installed",
-  }
-
-  package { "git-core":
-    ensure => installed,
+    ensure => 'installed',
+    require => Exec['apt-get-init'],
   }
 
   file { "/home/$username/.gitconfig":
     ensure  => file,
     owner   => $username,
     group   => $username,
-    source  => "puppet:///modules/dev-base/.gitconfig",
+    source  => 'puppet:///modules/dev-base/.gitconfig',
     require => [User[$username], Package['git-core']],
   }
 
--- puppet/modules/system-base/manifests/init.pp
+++ puppet/modules/system-base/manifests/init.pp
@@ -1,10 +1,39 @@
 
 class system-base {
-  
+
+  file {'/tmp/neotechnology.gpg.key':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/system-base/neotechnology.gpg.key',
+  }
+
+  exec { "Import neotechnology key to apt keystore":
+    path        => '/bin:/usr/bin',
+    environment => 'HOME=/root',
+    command     => 'apt-key add /tmp/neotechnology.gpg.key',
+    user        => 'root',
+    group       => 'root',
+    unless      => 'apt-key list | grep neotechnology',
+    logoutput   => on_failure,
+    require     => File['/tmp/neotechnology.gpg.key'],
+  }
+
+  file {'/etc/apt/sources.list.d/neo4j.list':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/system-base/neo4j.list',
+  }
+
+
   exec { 'apt-get-init':
-    path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    command => 'apt-get update',
-    refreshonly => true
+    path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    command     => 'apt-get update',
+    refreshonly => true,
+    require     => File['/etc/apt/sources.list.d/neo4j.list'],
   }
 
 
