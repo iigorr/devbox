6c2ab8b4ee6448e02f168080ae13f097ff7dd67f
Igor Lankin <dev@lankin.net>
2014-07-06 01:33:40 +0200

    added configparser package

--- puppet/modules/dev-base/manifests/python.pp
+++ puppet/modules/dev-base/manifests/python.pp
@@ -1,7 +1,7 @@
 class dev-base::python {
 
   $packages = [ 'python-dev']
-  $pip_packages = [ 'sh', 'fabric' ]
+  $pip_packages = [ 'sh', 'fabric', 'configparser' ]
   package { $packages:
     ensure  => 'installed',
     require => Exec['apt-get-init'],
